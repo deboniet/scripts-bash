7a27276073256184f33f5aa826690dede5b814f2
deboniet <178730704+deboniet@users.noreply.github.com>
2024-08-18 22:40:21 +0200

    Add files via upload

---
+++ vmwareinstall.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (C) 2024 deboniet
+
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+##################################################
+# Descripción del script: Instalar o actualizar VMware Workstation.
+# Compatibilidad: CPUs x86 de 64 bits que ejecuten Debian 11 o superior, o Ubuntu 20.04 o superior. Es también compatible con cualquier otra distribución que use los repositorios de alguna de estas dos distribuciones.
+##################################################
+# Debido a que el ejecutable que se descargue es de un solo uso y de gran tamaño, se descargara en /tmp, de manera que al reinicio ya no esté almacenado.
+cd /tmp
+# Esto permitirá ejecutar comandos con permisos de súper-usuario sin necesidad de poner la contraseña una y otra vez. Si has configurado sudoers para que este tipo de "trucos" no funcione, esto será inútil.
+sudo echo 1>/dev/null
+echo "Comprobando si es necesario instalar paquetes."
+sudo apt update 1>/dev/null 2>/dev/null
+sudo apt -y install coreutils openssl mokutil wget tar
+clear
+# Obtención y descompresión de la última versión del instalador de VMware Workstation.
+echo "Descargando la versión más reciente del instalador. Espera."
+wget https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.5.2/23775571/linux/core/VMware-Workstation-17.5.2-23775571.x86_64.bundle.tar 1>/dev/null 2>/dev/null
+tar -xf VMware-Workstation-17.5.2-23775571.x86_64.bundle.tar
+chmod 700 VMware-Workstation-17.5.2-23775571.x86_64.bundle
+echo
+# Desinstalar VMware Workstation y re-instalarlo.
+echo "Q para cancelar, ENTER para seguir."
+echo "(No hace falta pulsar ninguna tecla si no estaba previamente instalado)"
+sudo vmware-installer -u vmware-workstation 1>/dev/null 2>/dev/null
+echo "Instalando VMware Workstation 17."
+sudo /tmp/VMware-Workstation-17.5.2-23775571.x86_64.bundle 1>/dev/null
+echo "VMware Workstation 17 instalado."
+echo
+# Firmar los módulos vmmon y vmnet con una duración de un siglo.
+# NOTA: Algunos firmware no son compatibles con MOKs que tienen una longitud de clave de 4096 bits, si eso ocurriera, bajar el parámetro rsa a 2048.
+echo "Creando certificados para vmmon y vmnet."
+openssl req -new -x509 -newkey rsa:4096 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36525 -subj "/CN=VMware/" 2>/dev/null
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
+# Introducir una clave para añadir las firmas en el reinicio.
+echo "Introduce la clave para poder añadir el certificado en el reinicio:"
+sudo mokutil --import MOK.der
+mokutil --export ~/Descargas
+echo
+echo "Se han exportado las claves antiguas en Descargas, para posterior eliminación de las antiguas claves. Revisa el MOK antiguo y eliminalo. Después, reinicia el equipo para completar todas las operaciones en un solo reinicio."
