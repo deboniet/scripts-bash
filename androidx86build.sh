e9026556d46ce5cf203fd4ade64d318127b64876
deboniet <178730704+deboniet@users.noreply.github.com>
2025-03-06 23:06:15 +0100

    Copyright change

--- androidx86build.sh
+++ androidx86build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2024 deboniet
+# Copyright (C) 2024-2025 deboniet
 
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
 
@@ -13,8 +13,8 @@
 # Instrucciones de compilación basadas en: www.android-x86.org/source.html
 # Compatibilidad: CPUs x86 de 64 bits que ejecuten Debian 11 o superior, o Ubuntu 20.04 o superior. Es también compatible con cualquier otra distribución que use los repositorios de alguna de estas dos distribuciones.
 # Requisitos mínimos calculados: 202 GiB de espacio y 15,4 GiB de RAM (menos de esta cantidad puede ocasionar fallos al compilar ciertos componentes).
-# Recomendaciones previas: Consultar el espacio disponible con el comando df y tener un fichero de memoria de intercambio de al menos la mitad de la memoria RAM.
-# NOTA: El dominio de www.android-x86.org así como el dominio del código fuente no siempre están disponibles, por lo que puede que el script no funcione en ciertos momentos.
+# Recomendaciones previas: Consultar el espacio disponible con el comando df y tener un fichero de memoria de intercambio de, al menos, la mitad de la memoria RAM.
+# NOTA: El dominio de www.android-x86.org así como el dominio que aloja el código fuente no siempre están disponibles, por lo que puede que el script no funcione en ciertos momentos.
 ##################################################
 # Esto permitirá ejecutar comandos con permisos de súper-usuario sin necesidad de poner la contraseña una y otra vez. Si has configurado sudoers para que este tipo de "trucos" no funcione, esto será inútil.
 sudo echo 1>/dev/null
@@ -22,7 +22,7 @@ echo "Comprobando si es necesario instalar paquetes."
 # En Debian es necesario tener habilitados los paquetes de la rama contrib.
 sudo apt update 1>/dev/null 2>/dev/null
 sudo apt -y install git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses6 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig repo gcc make m4 lib32stdc++6 libelf-dev mtools libssl-dev python3-mako syslinux-utils openssh-client python-is-python3 pkgconf genisoimage squashfs-tools coreutils bash libncurses5 libselinux1-dev libsepol-dev
-# Comprobación para saber que versión de Java instalar, al no estar todavía OpenJDK 21 en los repositorios de Debian estable.
+# Comprobación para saber que versión de Java instalar al no estar todavía OpenJDK 21 en los repositorios de Debian estable.
 version=$(cat /etc/os-release | grep VERSION_ID | cut -c 13-14,16-17)
 distribucion=$(cat /etc/os-release | grep -w ID | cut -c 4-)
 if [ $version -ge 11 -a $distribucion == debian ];
@@ -71,7 +71,7 @@ repo sync --no-tags --no-clone-bundle
 source build/envsetup.sh
 lunch $arquitectura-$target
 echo
-# El parámetro nproc usará todos los núcleos del procesador para la compilación, lo que puede llegar a saturar el sistema en ciertos momentos. Otra opción recomendable puede ser: $(($(nproc) - 1)), que usa todos los núcleos excepto uno.
+# El parámetro nproc usará todos los núcleos del procesador para la compilación, lo que puede llegar a saturar el sistema en ciertos momentos. Otra opción recomendable puede ser: $(($(nproc) - 1)) que usa todos los núcleos excepto uno.
 make -j$(nproc) iso_img
 # Mover el resultado final, dependiendo de la arquitectura elegida anteriormente.
 if [ $arquitectura == android_x86_64 ];
