bf7815cd09f3eb7dcad9c7d358edf519078cb4e4
deboniet <178730704+deboniet@users.noreply.github.com>
2025-03-06 23:29:21 +0100

    Copyright change and update for the new installer of version 17.6.3

--- vmwareinstall.sh
+++ vmwareinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2024 deboniet
+# Copyright (C) 2024-2025 deboniet
 
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
 
@@ -11,6 +11,7 @@
 ##################################################
 # Descripción del script: Instalar o actualizar VMware Workstation.
 # Compatibilidad: CPUs x86 de 64 bits que ejecuten Debian 11 o superior, o Ubuntu 20.04 o superior. Es también compatible con cualquier otra distribución que use los repositorios de alguna de estas dos distribuciones.
+# NOTA: El script está pensado para usarse en equipos con Secure Boot activado (Lineas 38-50 del script).
 ##################################################
 # Debido a que el ejecutable que se descargue es de un solo uso y de gran tamaño, se descargara en /tmp, de manera que al reinicio ya no esté almacenado.
 cd /tmp
@@ -22,19 +23,19 @@ sudo apt -y install coreutils openssl mokutil wget tar sudo bash
 clear
 # Obtención y descompresión de la última versión del instalador de VMware Workstation.
 echo "Descargando la versión más reciente del instalador. Espera."
-wget https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.2/24409262/linux/core/VMware-Workstation-17.6.2-24409262.x86_64.bundle.tar 1>/dev/null 2>/dev/null
-tar -xf VMware-Workstation-17.6.2-24409262.x86_64.bundle.tar
-chmod 700 VMware-Workstation-17.6.2-24409262.x86_64.bundle
+wget https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.3/24583834/linux/core/VMware-Workstation-17.6.3-24583834.x86_64.bundle.tar 1>/dev/null 2>/dev/null
+tar -xf VMware-Workstation-17.6.3-24583834.x86_64.bundle.tar
+chmod 700 VMware-Workstation-17.6.3-24583834.x86_64.bundle
 echo
 # Desinstalar VMware Workstation y re-instalarlo.
 echo "Q para cancelar, ENTER para seguir."
 echo "(No hace falta pulsar ninguna tecla si no estaba previamente instalado)"
 sudo vmware-installer -u vmware-workstation 1>/dev/null 2>/dev/null
 echo "Instalando VMware Workstation 17."
-sudo /tmp/VMware-Workstation-17.6.2-24409262.x86_64.bundle 1>/dev/null
+sudo /tmp/VMware-Workstation-17.6.3-24583834.x86_64.bundle 1>/dev/null
 echo "VMware Workstation 17 instalado."
 echo
-# Firmar los módulos vmmon y vmnet, para que funcionen con SecureBoot activado, con una duración de un siglo.
+# Firmar los módulos vmmon y vmnet, para que funcionen con Secure Boot activado, con una duración de un siglo.
 # NOTA: Algunos firmware no son compatibles con MOKs que tienen una longitud de clave de 4096 bits, si eso ocurriera, bajar el parámetro rsa a 2048.
 echo "Creando el certificado para vmmon y vmnet."
 openssl req -new -x509 -newkey rsa:4096 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36525 -subj "/CN=VMware/" 2>/dev/null
