d0d4d1c87f5cfb3f9962ba3e9fb71e890cc877a8
deboniet <178730704+deboniet@users.noreply.github.com>
2025-04-14 12:15:37 +0200

    Working again as expected
    - Removal of any OSDN fetch. Now, Android-x86 files are downloaded from the GitHub and SourceForge mirrors of the developers.
    - Download manifests are now fetched from a repository forked by me and with fixes to work properly. This means that only the manifests are downloaded from my repository. The other files and source code are still downloaded from the official places.
    - The download of the source code should be lighter and faster now.
    - Built-in mechanisms to evade posible errors during downloads. If an error ocurrs, the script can run again without re-downloading the source code again and using the existing .repo folder.
    - Removal of unneeded transitional packages.
    - More comments to explain better what the script does.

--- androidx86build.sh
+++ androidx86build.sh
@@ -9,20 +9,20 @@
 # You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 ##################################################
-# Descripción del script: Crear una imagen ISO arrancable de Android para equipos x86.
+# Descripción del script: Crear una ISO arrancable de Android para equipos x86.
 # Instrucciones de compilación basadas en: www.android-x86.org/source.html
 # Compatibilidad: CPUs x86 de 64 bits que ejecuten Debian 11 o superior, o Ubuntu 20.04 o superior. Es también compatible con cualquier otra distribución que use los repositorios de alguna de estas dos distribuciones.
-# Requisitos mínimos calculados: 202 GiB de espacio y 15,4 GiB de RAM (menos de esta cantidad puede ocasionar fallos al compilar ciertos componentes).
-# Recomendaciones previas: Consultar el espacio disponible con el comando df y tener un fichero de memoria de intercambio de, al menos, la mitad de la memoria RAM.
-# NOTA: El dominio de www.android-x86.org así como el dominio que aloja el código fuente no siempre están disponibles, por lo que puede que el script no funcione en ciertos momentos.
+# Requisitos mínimos calculados para la rama r-x86: 182 GiB de espacio y 15 GiB de RAM (menos de esta cantidad puede ocasionar fallos al compilar ciertos componentes).
+# Recomendaciones previas: Consultar el espacio disponible en disco y tener un fichero de memoria de intercambio de, al menos, la mitad de la memoria RAM.
 ##################################################
 # Esto permitirá ejecutar comandos con permisos de súper-usuario sin necesidad de poner la contraseña una y otra vez. Si has configurado sudoers para que este tipo de "trucos" no funcione, esto será inútil.
 sudo echo 1>/dev/null
 echo "Comprobando si es necesario instalar paquetes."
 # En Debian es necesario tener habilitados los paquetes de la rama contrib.
 sudo apt update 1>/dev/null 2>/dev/null
-sudo apt -y install git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses6 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig repo gcc make m4 lib32stdc++6 libelf-dev mtools libssl-dev python3-mako syslinux-utils openssh-client python-is-python3 pkgconf genisoimage squashfs-tools coreutils bash libncurses5 libselinux1-dev libsepol-dev
+sudo apt -y install git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses6 libx11-dev lib32z1-dev libxml2-utils xsltproc unzip fontconfig repo gcc make m4 lib32stdc++6 libelf-dev mtools libssl-dev python3-mako syslinux-utils openssh-client python-is-python3 pkgconf genisoimage squashfs-tools coreutils bash libncurses5 libselinux1-dev libsepol-dev wget unzip
 # Comprobación para saber que versión de Java instalar al no estar todavía OpenJDK 21 en los repositorios de Debian estable.
+# NOTA: La compilación no da errores con estas versiones de Java, pero si da ciertas advertencias por usarlas. Si esto te preocupa, cambia a la versión 8 de Java.
 version=$(cat /etc/os-release | grep VERSION_ID | cut -c 13-14,16-17)
 distribucion=$(cat /etc/os-release | grep -w ID | cut -c 4-)
 if [ $version -ge 11 -a $distribucion == debian ];
@@ -32,21 +32,20 @@ else
 	sudo apt -y install openjdk-21-jdk
 fi
 clear
-echo "NOTA: Se creará automáticamente un sub-directorio en el lugar que especifiques a continuación."
+echo "NOTA: Se creará un directorio con la rama elegida en el lugar que especifiques a continuación."
 read -p "Introduce el directorio donde se alojara la compilación: " directorio
 cd "$directorio"
 echo
-read -p "Introduce el directorio donde se alojara el resultado final: " resultado
+read -p "Introduce el directorio donde se moverá la ISO: " resultado
 echo
-# El resto de versiones requieren de programas más antiguos para poder ser compiladas. Para más información, consulta el apartado "Building the image" en www.android-x86.org/source.html
+# El resto de versiones requieren de programas muy antiguos, o repositorios inexistentes, para poder ser compiladas. Para más información, consulta el apartado "Building the image" en www.android-x86.org/source.html
 echo "Versiones disponibles:"
 echo "r-x86 --> Android 11"
 echo "q-x86 --> Android 10"
 echo "pie-x86 --> Android 9"
 echo "oreo-x86 --> Android 8.1"
-echo "nougat-x86 --> Android 7.1"
-echo "marshmallow-x86 --> Android 6"
-echo "lollipop-x86 --> Android 5.1"
+echo "nougat-x86 --> Android 7.1 (Requiere Java 8)"
+echo "marshmallow-x86 --> Android 6 (Requiere Java 8)"
 echo
 read -p "Introduce la versión: " android
 echo
@@ -63,11 +62,61 @@ echo "eng --> Acceso ilimitado, ideal para desarrollo."
 echo
 read -p "Introduce el target: " target
 echo
-mkdir android-x86 2>/dev/null && cd android-x86
+mkdir $android 2>/dev/null
+cd $android
+# Descarga, desde SourceForge, del kernel y otros ficheros, al no estar disponible en el espejo de GitHub.
+echo "Descargando y extrayendo los ficheros necesarios para la rama $android. Espera."
+if [ $android = r-x86 ];
+then
+	rm -r kernel 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/kernel.git/android-x86-kernel-227c2c1aa5184f2cd1872d079ab9d96790bcaf69.zip 2>/dev/null
+	unzip -q android-x86-kernel-227c2c1aa5184f2cd1872d079ab9d96790bcaf69.zip
+	mv android-x86-kernel-227c2c1aa5184f2cd1872d079ab9d96790bcaf69 kernel
+	rm android-x86-kernel-227c2c1aa5184f2cd1872d079ab9d96790bcaf69.zip
+elif [ $android = q-x86 -o $android = pie-x86 -o $android = oreo-x86 ];
+then
+	rm -r kernel 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/kernel.git/android-x86-kernel-0676905e8791e9a838216e02d4974b2c965e3d4b.zip 2>/dev/null
+	unzip -q android-x86-kernel-0676905e8791e9a838216e02d4974b2c965e3d4b.zip
+	mv android-x86-kernel-0676905e8791e9a838216e02d4974b2c965e3d4b kernel
+	rm android-x86-kernel-0676905e8791e9a838216e02d4974b2c965e3d4b.zip
+elif [ $android = nougat-x86 ];
+then
+	# Primero el kernel.
+	rm -r kernel 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/kernel.git/android-x86-kernel-dcaac9a77ef90bf7844559838a032b4dfd4db32c.zip 2>/dev/null
+	unzip -q android-x86-kernel-dcaac9a77ef90bf7844559838a032b4dfd4db32c.zip
+	mv android-x86-kernel-dcaac9a77ef90bf7844559838a032b4dfd4db32c kernel
+	rm android-x86-kernel-dcaac9a77ef90bf7844559838a032b4dfd4db32c.zip
+	# Después los ficheros de construcción.
+	rm -r build 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/build.git/android-x86-build-a5794035de9c287fe79404df4beb41575e6c23bd.zip 2>/dev/null
+	unzip -q android-x86-build-a5794035de9c287fe79404df4beb41575e6c23bd.zip
+	mv android-x86-build-a5794035de9c287fe79404df4beb41575e6c23bd build
+	mv build/core/root.mk Makefile
+	rm android-x86-build-a5794035de9c287fe79404df4beb41575e6c23bd.zip
+elif [ $android = marshmallow-x86 ];
+then
+	# Primero el kernel.
+	rm -r kernel 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/kernel.git/android-x86-kernel-c484ab103fe93254f4a6af7f63275febe607d88b.zip 2>/dev/null
+	unzip -q android-x86-kernel-c484ab103fe93254f4a6af7f63275febe607d88b.zip
+	mv android-x86-kernel-c484ab103fe93254f4a6af7f63275febe607d88b kernel
+	rm android-x86-kernel-c484ab103fe93254f4a6af7f63275febe607d88b.zip
+	# Después los ficheros de construcción.
+	rm -r build 2>/dev/null
+	wget https://sourceforge.net/code-snapshots/git/a/an/android-x86/build.git/android-x86-build-c17b3126024edc56736a50ed9401ed0f7b8ca37f.zip 2>/dev/null
+	unzip -q android-x86-build-c17b3126024edc56736a50ed9401ed0f7b8ca37f.zip
+	mv android-x86-build-c17b3126024edc56736a50ed9401ed0f7b8ca37f build
+	mv build/core/root.mk Makefile
+	rm android-x86-build-c17b3126024edc56736a50ed9401ed0f7b8ca37f.zip
+fi
 # Estos dos comandos son necesarios para la ejecución de Git. Si tienes configurado estos parámetros, comenta la siguiente línea.
 git config --global user.email "" && git config --global user.name ""
-repo init -u http://scm.osdn.net/gitroot/android-x86/manifest -b $android
-repo sync --no-tags --no-clone-bundle
+echo
+# Reemplazo de los antiguos manifiestos por unos funcionales.
+repo init --partial-clone -b $android -u https://github.com/deboniet/android-x86-manifest
+repo sync -c --no-tags --no-clone-bundle -j$(nproc)
 source build/envsetup.sh
 lunch $arquitectura-$target
 echo
