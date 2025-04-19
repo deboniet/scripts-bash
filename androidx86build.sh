fd04491dfb179a4ecc920296688b85cfbada7bbc
deboniet <178730704+deboniet@users.noreply.github.com>
2025-04-19 18:01:53 +0200

    General and q-x86 branch fixes
    General:
    - The repo sync stage is now done using the latest repo version downloaded from Google.
    - More built-in mechanisms to evade errors after downloads.
    
    q-x86:
    - Added gettext to the packages list as it's necessary for this branch.
    - Added an extra download with modified files so compilation can finish successfully.

--- androidx86build.sh
+++ androidx86build.sh
@@ -20,12 +20,12 @@ sudo echo 1>/dev/null
 echo "Comprobando si es necesario instalar paquetes."
 # En Debian es necesario tener habilitados los paquetes de la rama contrib.
 sudo apt update 1>/dev/null 2>/dev/null
-sudo apt -y install git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses6 libx11-dev lib32z1-dev libxml2-utils xsltproc unzip fontconfig repo gcc make m4 lib32stdc++6 libelf-dev mtools libssl-dev python3-mako syslinux-utils openssh-client python-is-python3 pkgconf genisoimage squashfs-tools coreutils bash libncurses5 libselinux1-dev libsepol-dev wget unzip
+sudo apt -y install git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses6 libx11-dev lib32z1-dev libxml2-utils xsltproc unzip fontconfig repo gcc make m4 lib32stdc++6 libelf-dev mtools libssl-dev python3-mako syslinux-utils openssh-client python-is-python3 pkgconf genisoimage squashfs-tools coreutils bash libncurses5 libselinux1-dev libsepol-dev wget unzip gettext
 # Comprobación para saber que versión de Java instalar al no estar todavía OpenJDK 21 en los repositorios de Debian estable.
 # NOTA: La compilación no da errores con estas versiones de Java, pero si da ciertas advertencias por usarlas. Si esto te preocupa, cambia a la versión 8 de Java.
 version=$(cat /etc/os-release | grep VERSION_ID | cut -c 13-14,16-17)
 distribucion=$(cat /etc/os-release | grep -w ID | cut -c 4-)
-if [ $version -ge 11 -a $distribucion == debian ];
+if [ $version -ge 11 -a $distribucion = debian ];
 then
 	sudo apt -y install openjdk-17-jdk
 else
@@ -66,9 +66,12 @@ mkdir $android 2>/dev/null
 cd $android
 # Descarga, desde Internet Archive, del kernel y otros ficheros, al no estar disponibles en el espejo de GitHub.
 echo "Descargando y extrayendo los ficheros necesarios para la rama $android. Espera."
+# Las descargas desde Internet Archive pueden ser lentas a veces. La solución está en reintentarlo hasta que se consiga una velocidad de descarga decente.
+echo "NOTA: Si la descarga es muy lenta, prueba a ejecutar de nuevo el script."
 if [ $android = r-x86 ];
 then
 	rm -r kernel 2>/dev/null
+	rm r-x86_kernel.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/r-x86_kernel.zip 2>/dev/null
 	unzip -q r-x86_kernel.zip
 	mv android-x86-kernel-227c2c1aa5184f2cd1872d079ab9d96790bcaf69 kernel
@@ -76,6 +79,7 @@ then
 elif [ $android = q-x86 -o $android = pie-x86 -o $android = oreo-x86 ];
 then
 	rm -r kernel 2>/dev/null
+	rm q-pie-oreo-x86_kernel.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/q-pie-oreo-x86_kernel.zip 2>/dev/null
 	unzip -q q-pie-oreo-x86_kernel.zip
 	mv android-x86-kernel-0676905e8791e9a838216e02d4974b2c965e3d4b kernel
@@ -84,12 +88,14 @@ elif [ $android = nougat-x86 ];
 then
 	# Primero el kernel.
 	rm -r kernel 2>/dev/null
+	rm nougat-x86_kernel.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/nougat-x86_kernel.zip 2>/dev/null
 	unzip -q nougat-x86_kernel.zip
 	mv android-x86-kernel-dcaac9a77ef90bf7844559838a032b4dfd4db32c kernel
 	rm nougat-x86_kernel.zip
 	# Después los ficheros de construcción.
 	rm -r build 2>/dev/null
+	rm nougat-x86_build.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/nougat-x86_build.zip 2>/dev/null
 	unzip -q nougat-x86_build.zip
 	mv android-x86-build-a5794035de9c287fe79404df4beb41575e6c23bd build
@@ -99,12 +105,14 @@ elif [ $android = marshmallow-x86 ];
 then
 	# Primero el kernel.
 	rm -r kernel 2>/dev/null
+	rm marshmallow-x86_kernel.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/marshmallow-x86_kernel.zip 2>/dev/null
 	unzip -q marshmallow-x86_kernel.zip
 	mv android-x86-kernel-c484ab103fe93254f4a6af7f63275febe607d88b kernel
 	rm marshmallow-x86_kernel.zip
 	# Después los ficheros de construcción.
 	rm -r build 2>/dev/null
+	rm marshmallow-x86_build.zip 2>/dev/null
 	wget https://archive.org/download/androidx86-build-files/marshmallow-x86_build.zip 2>/dev/null
 	unzip -q marshmallow-x86_build.zip
 	mv android-x86-build-c17b3126024edc56736a50ed9401ed0f7b8ca37f build
@@ -116,17 +124,34 @@ git config --global user.email "" && git config --global user.name ""
 echo
 # Reemplazo de los antiguos manifiestos por unos funcionales.
 repo init --partial-clone -b $android -u https://github.com/deboniet/android-x86-manifest
-repo sync -c --no-tags --no-clone-bundle -j$(nproc)
+# Usar la última versión de repo descargada de Google, en vez de la instalada por la distribución.
+.repo/repo/repo sync -c --no-tags --no-clone-bundle -j$(nproc)
+echo
+# Descarga de ficheros modificados, necesarios para una compilación correcta.
+if [ $android = q-x86 ];
+then
+	echo "Descargando y extrayendo ficheros modificados para la rama q-x86. Espera."
+	rm -r q-x86_mod-files 2>/dev/null
+	rm q-x86_mod-files.zip 2>/dev/null
+	wget https://archive.org/download/androidx86-build-files/q-x86_mod-files.zip 2>/dev/null
+	unzip -q q-x86_mod-files.zip
+	mv q-x86_mod-files/gl_XML.py external/mesa/src/mapi/glapi/gen
+	mv q-x86_mod-files/glX_XML.py external/mesa/src/mapi/glapi/gen
+	mv q-x86_mod-files/drmhwctwo.cpp external/drm_hwcomposer
+	mv q-x86_mod-files/config.go build/soong/ui/build/paths
+	rm -r q-x86_mod-files 2>/dev/null
+	rm q-x86_mod-files.zip 2>/dev/null
+fi
 source build/envsetup.sh
 lunch $arquitectura-$target
 echo
 # El parámetro nproc usará todos los núcleos del procesador para la compilación, lo que puede llegar a saturar el sistema en ciertos momentos. Otra opción recomendable puede ser: $(($(nproc) - 1)) que usa todos los núcleos excepto uno.
 make -j$(nproc) iso_img
 # Mover el resultado final, dependiendo de la arquitectura elegida anteriormente.
-if [ $arquitectura == android_x86_64 ];
+if [ $arquitectura = android_x86_64 ];
 then
 	mv out/target/product/x86_64/android_x86_64.iso "$resultado"
-elif [ $arquitectura == android_x86 ];
+elif [ $arquitectura = android_x86 ];
 then
 	mv out/target/product/x86/android_x86.iso "$resultado"
 fi
