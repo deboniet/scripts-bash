4d847bbebff9de0db7c4a08e32c3ae6e047bcbb0
deboniet <178730704+deboniet@users.noreply.github.com>
2025-04-24 16:16:33 +0200

    Update for the 2503a release

--- dolphinemubuild.sh
+++ dolphinemubuild.sh
@@ -9,24 +9,24 @@
 # You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 ##################################################
-# Descripción del script: Instalar o actualizar Doplhin Emulator.
+# Descripción del script: Instalar o actualizar Dolphin Emulator.
 # Instrucciones de compilación basadas en: github.com/dolphin-emu/dolphin/wiki/Building-for-Linux
 # Compatibilidad: CPUs x86 de 64 bits que ejecuten Debian 12 o superior, o Ubuntu 22.04 o superior. Es también compatible con cualquier otra distribución que use los repositorios de alguna de estas dos distribuciones.
 ##################################################
-# Debido a que la compilación será de un solo uso, se realizara en /tmp, de manera que al reinicio ya no esté almacenado.
+# Debido a que la compilación será de un solo uso, se realizará en /tmp, de manera que al reinicio ya no esté almacenado.
 cd /tmp
 # Esto permitirá ejecutar comandos con permisos de súper-usuario sin necesidad de poner la contraseña una y otra vez. Si has configurado sudoers para que este tipo de "trucos" no funcione, esto será inútil.
 sudo echo 1>/dev/null
 echo "Comprobando si es necesario instalar paquetes."
 sudo apt update 1>/dev/null 2>/dev/null
 sudo apt -y install build-essential curl git gcc cmake make ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libevdev-dev libusb-1.0-0-dev libxrandr-dev libxi-dev libpangocairo-1.0-0 qt6-base-private-dev libbluetooth-dev libasound2-dev libpulse-dev libgl1-mesa-dev libcurl4-openssl-dev libpipewire-0.3-dev libsystemd-dev libdrm-dev libsndio-dev libwayland-dev libzstd-dev liblz4-dev libsfml-dev libgtest-dev libsdl2-dev libbz2-dev liblzma-dev libpugixml-dev libcubeb-dev libmbedtls-dev libhidapi-dev libvulkan-dev libunwind-dev gettext coreutils sudo bash
-# Comprobación de versión para saber qué paquete instalar, ya que el nombre difiere entre Ubuntu y Debian.
+# Comprobación para saber qué paquete instalar, ya que el nombre difiere entre Ubuntu y Debian.
 version=$(cat /etc/os-release | grep VERSION_ID | cut -c 13-14,16-17)
 distribucion=$(cat /etc/os-release | grep -w ID | cut -c 4-)
-if [ $version -ge 12 -a $distribucion == debian ];
+if [ $version -ge 12 -a $distribucion = debian ];
 then
 	sudo apt -y install qt6-svg-dev
-elif [ $version -ge 2404 -a $distribucion == ubuntu ];
+elif [ $version -ge 2404 -a $distribucion = ubuntu ];
 then
 	sudo apt -y install qt6-svg-dev
 else
@@ -39,7 +39,7 @@ if [ $version = Estable -o $version = estable -o $version = E -o $version = e ];
 then
 	echo
 	echo "Seleccionada la versión estable. Descargando."
-	git clone --branch 2503 --single-branch https://github.com/dolphin-emu/dolphin
+	git clone --branch 2503a --single-branch https://github.com/dolphin-emu/dolphin
 elif [ $version = Desarrollo -o $version = desarrollo -o $version = D -o $version = d ];
 then
 	echo
