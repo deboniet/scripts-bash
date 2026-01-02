f78dde22610eba25b63ea654d3d83512646b0451
deboniet <178730704+deboniet@users.noreply.github.com>
2026-01-02 18:10:27 +0100

    Copyright change

--- cpuvuln.sh
+++ cpuvuln.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2024-2025 deboniet
+# Copyright (C) 2026 deboniet
 
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
 
